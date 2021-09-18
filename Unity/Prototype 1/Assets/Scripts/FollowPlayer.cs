a324a55a380bf56827dcb49b9e542d4de48121a7
ivan-arsenev <iv.arsenev@gmail.com>
2021-09-18 17:45:43 +0300

    1

---
+++ Unity/Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset = new Vector3(0, 5, -8);
+    void LateUpdate()
+    {
+        // Offset the camera behind the player by adding to the player's position
+        transform.position = player.transform.position + offset;
+    }
+}
