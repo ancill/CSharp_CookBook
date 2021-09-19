a5893a08687bdd5f8381af53785cb2d9e1585d78
Ancill <42762155+ancill@users.noreply.github.com>
2021-09-19 10:09:54 +0300

    Completed car project

--- Unity/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Unity/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
+    private float speed = 15.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
         
-        // Controll player movement
+        // Moves the car forward based on vertical input
         transform.Translate(Vector3.forward * (Time.deltaTime * speed * forwardInput));
-        transform.Translate(Vector3.right * (Time.deltaTime * turnSpeed * horizontalInput));
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up * (Time.deltaTime * turnSpeed * horizontalInput));
     }
 }
