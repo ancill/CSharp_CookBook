f8b0f32791036c80db0abdfcb6b55d90fee5cf62
Ancill <42762155+ancill@users.noreply.github.com>
2021-09-20 19:39:01 +0300

    Update SpawnManager.cs

--- Unity/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Unity/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -13,9 +13,9 @@ public class SpawnManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.S))
         {
             // Randomly generate animal index and spawn position
-            var spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
-                0, spawnPosZ);
+            var spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
             int animalIndex = Random.Range(0, animalPrefabs.Length);
+            
             Instantiate(animalPrefabs[animalIndex], spawnPos,
                 animalPrefabs[animalIndex].transform.rotation);
         }
