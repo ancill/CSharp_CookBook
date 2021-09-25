b432d1a3aa3f61cad629fdf9091a5b64f0cbf424
Ancill <42762155+ancill@users.noreply.github.com>
2021-09-25 14:46:25 +0300

    Implement fizzBuzz game

---
+++ Exercise/3.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+namespace _3._2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        checked
+        {
+          int max = 500;
+          for (byte i = 0; i < max; i++)
+          {
+            WriteLine(i);
+          }
+        }
+      }
+      catch (OverflowException)
+      {
+        WriteLine("The code overflowed but I caught the exception.");
+      }
+      catch (Exception ex)
+      {
+        WriteLine($"{ex.GetType()} says {ex.Message}");
+
+      }
+
+    }
+  }
+}
---
+++ Exercise/3.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+namespace _3._3
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      WriteLine("Simple FizzBuzz game!");
+
+      for (int i = 0; i < 100; ++i)
+      {
+        var answer = "";
+        if (i % 3 == 0) answer = "Fizz";
+        if (i % 5 == 0) answer = "Buzz";
+        if (i % 5 == 0 && i % 3 == 0) answer = "FizzBuzz";
+        answer = String.IsNullOrEmpty(answer) ? i.ToString() : answer;
+        Write($"{answer}\t");
+      }
+      WriteLine();
+    }
+  }
+}
