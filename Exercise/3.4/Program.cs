6c3438d27ad7c49f1875062a1201521271039ef9
Ancill <42762155+ancill@users.noreply.github.com>
2021-09-25 15:21:49 +0300

    Started 4 chapter

---
+++ Exercise/3.4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+namespace _3._4
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      try
+      {
+        WriteLine($"Enter a number between 0 and 255:");
+        var input1 = uint.Parse(ReadLine());
+        WriteLine("Enter another number between 0 and 255:");
+        var input2 = uint.Parse(ReadLine());
+        if (input1 > 255 || input2 > 255) throw new OverflowException();
+        WriteLine($"{input1} divided by {input2} is {input1 / input2}");
+      }
+      catch (FormatException FE)
+      {
+        WriteLine($"{FE.GetType()}: Input string was not in a correct format.");
+      }
+      catch (DivideByZeroException)
+      {
+        WriteLine("Divided by zero!");
+      }
+      catch (OverflowException)
+      {
+        WriteLine("Provided number less then 0 or more then 255");
+      }
+      catch (Exception ex)
+      {
+        WriteLine(ex);
+      }
+    }
+  }
+}
---
+++ Exercise/3.5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _3._5
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      var x = 3;
+      var y = 2 + ++x;
+      x = 3 << 2;
+      y = 10 >> 1;
+      x = 10 & 8;
+      y = 10 | 7;
+      Console.WriteLine($"x value is {x} and y is {y}");
+    }
+  }
+}
