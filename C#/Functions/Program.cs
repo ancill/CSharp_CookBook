287ec03d7172bbce6284f02288d459c35b829a4a
Ancill <42762155+ancill@users.noreply.github.com>
2021-10-01 07:50:47 +0300

    Update Program.cs

--- C#/Functions/Program.cs
+++ C#/Functions/Program.cs
@@ -94,6 +94,31 @@ namespace Functions
       }
     }
 
+    /// <summary>
+    /// Pass a 32-bit integer and it will be converted into its ordinal equivalent.
+    /// </summary>
+    /// <param name="number">Number is a cardinal value e.g. 1, 2, 3, and so on.</param>
+    /// <returns>Number as an ordinal value e.g. 1st, 2nd, 3rd, and so on.</returns>
+    static string CardinalToOrdinal(int number)
+    {
+      switch (number)
+      {
+        case 11: // special cases for 11th to 13th case 12:
+        case 13:
+          return $"{number}th";
+        default:
+          int lastDigit = number % 10;
+          string suffix = lastDigit switch
+          {
+            1 => "st",
+            2 => "nd",
+            3 => "rd",
+            _ => "th"
+          };
+          return $"{number}{suffix}";
+      }
+    }
+
     static int FibImperative(int term)
     {
       if (term == 1)
@@ -109,20 +134,28 @@ namespace Functions
         return FibImperative(term - 1) + FibImperative(term - 2);
       }
     }
+    static int FibFunctional(int term) =>
+      term switch
+      {
+        1 => 0,
+        2 => 1,
+        _ => FibFunctional(term - 1) + FibFunctional(term - 2)
+      };
+
     static void RunFibImperative()
     {
       for (int i = 1; i <= 30; i++)
       {
         WriteLine("The {0} term of the Fibonacci sequence is {1:N0}.",
           arg0: CardinalToOrdinal(i),
-          arg1: FibImperative(term: i));
+          arg1: FibFunctional(term: i));
       }
     }
 
     static void Main(string[] args)
     {
       // RunTimesTable();
-      RunRecalculateTax();
+      RunFibImperative();
     }
   }
 
