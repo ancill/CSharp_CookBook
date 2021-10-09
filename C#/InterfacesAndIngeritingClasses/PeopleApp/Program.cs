0f24e09cdac6b92d62f7ceb8e6f0f1e8a3b37682
Ancill <42762155+ancill@users.noreply.github.com>
2021-10-09 12:59:00 +0300

    Update Program.cs

--- C#/InterfacesAndIngeritingClasses/PeopleApp/Program.cs
+++ C#/InterfacesAndIngeritingClasses/PeopleApp/Program.cs
@@ -4,9 +4,23 @@ namespace PeopleApp
 {
   class Program
   {
+
+    private static void Harry_Shout(object sender, EventArgs e)
+    {
+      Person p = (Person)sender;
+      WriteLine($"{p.Name} is this angry: {p.AngerLevel}.");
+    }
+
     static void Main(string[] args)
     {
       var harry = new Person { Name = "Harry" };
+      harry.Shout = Harry_Shout;
+      harry.Poke();
+      harry.Poke();
+      harry.Poke();
+      harry.Poke();
+
+
       var mary = new Person { Name = "Mary" };
       var jill = new Person { Name = "Jill" };
       // call instance method
@@ -24,12 +38,6 @@ namespace PeopleApp
         arg1: harry.Children[0].Name);
 
       WriteLine($"5! is {Person.Factorial(50)}");
-
-      private static void Harry_Shout(object sender, EventArgs e)
-      {
-        Person p = (Person)sender;
-        WriteLine($"{p.Name} is this angry: {p.AngerLevel}.");
-      }
 
 
     }
