91d2356f99b2cdbb23f4d10d5492d050530aefb1
Ancill <42762155+ancill@users.noreply.github.com>
2021-10-18 22:23:09 +0300

    Update Program.cs

--- DotNet/WorkingWithFileSystems/Program.cs
+++ DotNet/WorkingWithFileSystems/Program.cs
@@ -33,4 +33,26 @@ public class Programm
     WriteLine("{0,-33} {1}", " .Personal)",
       GetFolderPath(SpecialFolder.Personal));
   }
+  //To manage drives, use DriveInfo, which has a static method that returns information about all the drives connected to your computer. Each drive has a drive type.
+  static void WorkWithDrives()
+  {
+    WriteLine("{0,-30} | {1,-10} | {2,-7} | {3,18} | {4,18}",
+      "NAME", "TYPE", "FORMAT", "SIZE (BYTES)", "FREE SPACE");
+    foreach (DriveInfo drive in DriveInfo.GetDrives())
+    {
+      if (drive.IsReady)
+      {
+        WriteLine(
+                "{0,-30} | {1,-10} | {2,-7} | {3,18:N0} | {4,18:N0}",
+                drive.Name, drive.DriveType, drive.DriveFormat,
+                drive.TotalSize, drive.AvailableFreeSpace);
+      }
+
+      else
+      {
+        WriteLine("{0,-30} | {1,-10}", drive.Name, drive.DriveType);
+      }
+    }
+
+  }
 }
