72dc226a76181f38edff8297f2f8da19e2e845bc
Ancill <42762155+ancill@users.noreply.github.com>
2021-10-19 00:00:04 +0300

    Update Program.cs

--- DotNet/WorkingWithStreams/Program.cs
+++ DotNet/WorkingWithStreams/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System.IO;
 using static System.Console;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static System.Environment;
+using static System.Console;
 
 public class Programm
 {
