33416d6b74e30a3f79d1186d7ce9a9467f5a3b1c
Ancill <42762155+ancill@users.noreply.github.com>
2021-10-18 23:29:32 +0300

    1

--- DotNet/WorkingWithFileSystems/Program.cs
+++ DotNet/WorkingWithFileSystems/Program.cs
@@ -8,7 +8,10 @@ public class Programm
 {
   public static void Main()
   {
-    OutputFileSystemInfo();
+    // OutputFileSystemInfo();
+    // WorkWithDrives();
+    // WorkWithDirectories();
+    WorkWithFiles();
   }
 
   static void OutputFileSystemInfo()
@@ -54,5 +57,75 @@ public class Programm
       }
     }
 
+  }
+
+  static void WorkWithDirectories()
+  {
+    // define a directory path for a new folder // starting in the user's folder
+    var newFolder = Combine(
+        GetFolderPath(SpecialFolder.Personal),
+        "Code", "Chapter09", "NewFolder");
+    WriteLine($"Working with: {newFolder}"); // check if it exists
+    WriteLine($"Does it exist? {Exists(newFolder)}");
+    // create directory
+    WriteLine("Creating it...");
+    CreateDirectory(newFolder);
+    WriteLine($"Does it exist? {Exists(newFolder)}");
+    Write("Confirm the directory exists, and then press ENTER: ");
+    ReadLine();
+    // delete directory
+    WriteLine("Deleting it...");
+    Delete(newFolder, recursive: true);
+    WriteLine($"Does it exist? {Exists(newFolder)}");
+  }
+
+  static void WorkWithFiles()
+  {
+    // define a directory path to output files // starting in the user's folder
+    var dir = Combine(
+        GetFolderPath(SpecialFolder.Personal),
+        "Code", "Chapter09", "OutputFiles");
+    CreateDirectory(dir);
+    // define file paths
+    string textFile = Combine(dir, "Dummy.txt");
+    string backupFile = Combine(dir, "Dummy.bak");
+    WriteLine($"Working with: {textFile}");
+    // check if a file exists
+    WriteLine($"Does it exist? {File.Exists(textFile)}");
+    // create a new text file and write a line to it
+    StreamWriter textWriter = File.CreateText(textFile);
+    textWriter.WriteLine("Hello, C#!");
+    textWriter.Close(); // close file and release resources
+    WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+    // copy the file, and overwrite if it already exists
+    File.Copy(sourceFileName: textFile,
+      destFileName: backupFile, overwrite: true);
+    WriteLine(
+      $"Does {backupFile} exist? {File.Exists(backupFile)}");
+    Write("Confirm the files exist, and then press ENTER: ");
+    ReadLine();
+    // delete file
+    File.Delete(textFile);
+    WriteLine($"Does it exist? {File.Exists(textFile)}");
+    // read from the text file backup
+    WriteLine($"Reading contents of {backupFile}:");
+    StreamReader textReader = File.OpenText(backupFile);
+    WriteLine(textReader.ReadToEnd());
+    textReader.Close();
+    // Managing paths
+    WriteLine($"Folder Name: {GetDirectoryName(textFile)}");
+    WriteLine($"File Name: {GetFileName(textFile)}");
+    WriteLine("File Name without Extension: {0}",
+      GetFileNameWithoutExtension(textFile));
+    WriteLine($"File Extension: {GetExtension(textFile)}");
+    WriteLine($"Random File Name: {GetRandomFileName()}");
+    WriteLine($"Temporary File Name: {GetTempFileName()}");
+
+    var info = new FileInfo(backupFile);
+    WriteLine($"{backupFile}:");
+    WriteLine($"Contains {info.Length} bytes");
+    WriteLine($"Last accessed {info.LastAccessTime}");
+    WriteLine($"Has readonly set to {info.IsReadOnly}");
   }
 }
