d90d8e66898c5c151aa2a496f3d7684ce6eafe3f
Ancill <42762155+ancill@users.noreply.github.com>
2021-10-20 23:54:45 +0300

    1

--- DotNet/WorkingWithStreams/Program.cs
+++ DotNet/WorkingWithStreams/Program.cs
@@ -84,54 +84,9 @@ public class Programm
     }
   }
 
-  static void WorkWithCompression()
-  {
-    string gzipFilePath = Combine(
-      CurrentDirectory, "streams.gzip");
-    FileStream gzipFile = File.Create(gzipFilePath);
-    using (GZipStream compressor = new GZipStream(
-      gzipFile, CompressionMode.Compress))
-    {
-      using (XmlWriter xmlGzip = XmlWriter.Create(compressor))
-      {
-        xmlGzip.WriteStartDocument();
-        xmlGzip.WriteStartElement("callsigns");
-        foreach (string item in callsigns)
-        {
-          xmlGzip.WriteElementString("callsign", item);
-        }
-        // the normal call to WriteEndElement is not necessary
-        // because when the XmlWriter disposes, it will
-        // automatically end any elements of any depth
-      }
-    } // also closes the underlying stream
-      // output all the contents of the compressed file
-    WriteLine("{0} contains {1:N0} bytes.",
-      gzipFilePath, new FileInfo(gzipFilePath).Length);
-    WriteLine($"The compressed contents:");
-    WriteLine(File.ReadAllText(gzipFilePath));
-    // read a compressed file
-    WriteLine("Reading the compressed XML file:");
-    gzipFile = File.Open(gzipFilePath, FileMode.Open);
-    using (GZipStream decompressor = new GZipStream(
-      gzipFile, CompressionMode.Decompress))
-    {
-      using (XmlReader reader = XmlReader.Create(decompressor))
-      {
-        while (reader.Read()) // read the next XML node {
-                              // check if we are on an element node named callsign
-          if ((reader.NodeType == XmlNodeType.Element)
-            && (reader.Name == "callsign"))
-          {
-            reader.Read(); // move to the text inside element
-            WriteLine($"{reader.Value}"); // read its value
-          }
-      }
-    }
-  }
-
   static void WorkWithCompression(bool useBrotli = true)
   {
+    System.Console.WriteLine($" Brotli: {useBrotli}");
     string fileExt = useBrotli ? "brotli" : "gzip";
     // compress the XML output
     string filePath = Combine(CurrentDirectory, $"streams.{fileExt}");
@@ -186,7 +141,8 @@ public class Programm
           if ((reader.NodeType == XmlNodeType.Element)
             && (reader.Name == "callsign"))
           {
-            reader.Read(); // move to the text inside element WriteLine($"{reader.Value}"); // read its value
+            reader.Read(); // move to the text inside element
+            WriteLine($"{reader.Value}"); // read its value
           }
         }
       }
@@ -195,7 +151,7 @@ public class Programm
   public static void Main()
   {
     //WorkWithText();
-    WorkWithXml();
+    // WorkWithXml();
     WorkWithCompression();
   }
 }
