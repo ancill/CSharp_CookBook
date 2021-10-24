0399315cc3f723d248616bd4d8637dfbcabb159b
Ancill <42762155+ancill@users.noreply.github.com>
2021-10-24 17:11:01 +0300

    1

--- DotNet/WorkingWithEncryption/CryptographyLib/Protector.cs
+++ DotNet/WorkingWithEncryption/CryptographyLib/Protector.cs
@@ -28,6 +28,50 @@ namespace Packt.Shared
     // iterations must be at least 1000, we will use 2000
     private static readonly int iterations = 2000;
 
+    private static Dictionary<string, User> Users =
+      new Dictionary<string, User>();
+    public static User Register(
+      string username, string password)
+    {
+      // generate a random salt
+      var rng = RandomNumberGenerator.Create();
+      var saltBytes = new byte[16];
+      rng.GetBytes(saltBytes);
+      var saltText = Convert.ToBase64String(saltBytes);
+      // generate the salted and hashed password
+      var saltedhashedPassword = SaltAndHashPassword(
+        password, saltText);
+      var user = new User
+      {
+        Name = username,
+        Salt = saltText,
+        SaltedHashedPassword = saltedhashedPassword
+      };
+      Users.Add(user.Name, user);
+      return user;
+    }
+    public static bool CheckPassword(
+  string username, string password)
+    {
+      if (!Users.ContainsKey(username))
+      {
+        return false;
+      }
+      var user = Users[username];
+      // re-generate the salted and hashed password
+      var saltedhashedPassword = SaltAndHashPassword(
+        password, user.Salt);
+      return (saltedhashedPassword == user.SaltedHashedPassword);
+    }
+    private static string SaltAndHashPassword(
+      string password, string salt)
+    {
+      var sha = SHA256.Create();
+      var saltedPassword = password + salt;
+      return Convert.ToBase64String(
+        sha.ComputeHash(Encoding.Unicode.GetBytes(saltedPassword)));
+    }
+
     public static string Encrypt(
       string plainText, string password)
     {
@@ -37,7 +81,8 @@ namespace Packt.Shared
       var aes = Aes.Create(); // abstract class factory method
       var pbkdf2 = new Rfc2898DeriveBytes(
         password, salt, iterations);
-      aes.Key = pbkdf2.GetBytes(32); // set a 256-bit key aes.IV = pbkdf2.GetBytes(16); // set a 128-bit IV
+      aes.Key = pbkdf2.GetBytes(32); // set a 256-bit key
+      aes.IV = pbkdf2.GetBytes(16); // set a 128-bit IV
 
       using (var ms = new MemoryStream())
       {
--- DotNet/WorkingWithEncryption/Program.cs
+++ DotNet/WorkingWithEncryption/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
-
+using System.Security.Cryptography; // CryptographicException
+using Packt.Shared; // Protector
+using static System.Console;
 namespace WorkingWithEncryption
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Write("Enter a message that you want to encrypt: ");
+      string message = ReadLine();
+      Write("Enter a password: ");
+      string password = ReadLine();
+      string cryptoText = Protector.Encrypt(message, password);
+      WriteLine($"Encrypted text: {cryptoText}");
+      Write("Enter the password: ");
+      string password2 = ReadLine();
+      try
+      {
+        string clearText = Protector.Decrypt(cryptoText, password2);
+        WriteLine($"Decrypted text: {clearText}");
+      }
+      catch (CryptographicException ex)
+      {
+        WriteLine("{0}\nMore details: {1}",
+         arg0: "You entered the wrong password!",
+         arg1: ex.Message);
+      }
+      catch (Exception ex)
+      {
+        WriteLine("Non-cryptographic exception: {0}, {1}",
+          arg0: ex.GetType().Name,
+          arg1: ex.Message);
+      }
     }
+  }
 }
