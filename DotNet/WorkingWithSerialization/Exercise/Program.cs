174879dfb14038a3dabe84aba3fd0e18756132f4
Ancill <42762155+ancill@users.noreply.github.com>
2021-10-26 12:57:10 +0300

    Added new exercise

---
+++ DotNet/WorkingWithSerialization/Exercise/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using Shapes;
+using static System.Environment;
+using static System.IO.Path;
+using static System.Console;
+
+namespace Exercise
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // create a list of Shapes to serialize
+      var listOfShapes = new List<Shape>
+        {
+        new Circle { Colour = "Red", Radius = 2.5 },
+        new Rectangle { Colour = "Blue", Height = 20.0, Width = 10.0 },
+        new Circle { Colour = "Green", Radius = 8.0 },
+        new Circle { Colour = "Purple", Radius = 12.3 },
+        new Rectangle { Colour = "Blue", Height = 45.0, Width = 18.0 }
+        };
+      var serializerXml = new XmlSerializer(typeof(List<Shape>)); // create a file to write to
+      string path = Combine(CurrentDirectory, "shapes.xml");
+      using (FileStream stream = File.Create(path))
+      {
+        // serialize the object graph to the stream
+        serializerXml.Serialize(stream, listOfShapes);
+      }
+      WriteLine("Written {0:N0} bytes of XML to {1}",
+        arg0: new FileInfo(path).Length,
+        arg1: path);
+      WriteLine();
+      // Display the serialized object graph
+      WriteLine(File.ReadAllText(path));
+
+
+      using (FileStream fileXml = File.Open(path, FileMode.Open))
+      {
+        // deserialize and cast the object graph into a List of Shapes
+        List<Shape> loadedShapesXml = serializerXml.Deserialize(fileXml) as List<Shape>;
+        foreach (Shape item in loadedShapesXml)
+        {
+          WriteLine("{0} is {1} and has an area of {2:N2}",
+            item.GetType().Name, item.Colour, item.Area);
+        }
+      }
+    }
+  }
+}
---
+++ DotNet/WorkingWithSerialization/Exercise/Shape.cs
@@ -0,0 +1,118 @@
+
+namespace Shapes
+{
+  public abstract class Shape
+  {
+    // fields
+    // protected - only inside this class and class that inherit it
+    protected double height;
+    protected double width;
+    protected string colour;
+
+    // properties
+    public virtual double Height
+    {
+      get
+      {
+        return height;
+      }
+      set
+      {
+        height = value;
+      }
+    }
+
+    public virtual double Width
+    {
+      get
+      {
+        return width;
+      }
+      set
+      {
+        width = value;
+      }
+    }
+
+    // Area must be implemented by derived classes
+    // as a read-only property
+    public virtual double Area { get; }
+
+    public virtual string Colour
+    {
+      get
+      {
+        return colour;
+      }
+      set
+      {
+        colour = value;
+      }
+    }
+
+  }
+
+
+  public class Rectangle : Shape
+  {
+    public Rectangle() { }
+
+    public Rectangle(double height, double width)
+    {
+      this.height = height;
+      this.width = width;
+    }
+
+    public override double Area
+    {
+      get
+      {
+        return height * width;
+      }
+    }
+  }
+
+  public class Square : Shape
+  {
+    public Square() { }
+    public Square(double width)
+    {
+      this.height = width;
+      this.width = width;
+    }
+    public override double Area
+    {
+      get
+      {
+        return height * width;
+      }
+    }
+  }
+
+  public class Circle : Square
+  {
+    public Circle() { }
+    public Circle(double radius) : base(width: radius * 2) { }
+    public double Radius
+    {
+      get
+      {
+        return height / 2;
+      }
+      set
+      {
+        Height = value * 2;
+      }
+    }
+
+    public override double Area
+    {
+      get
+      {
+        var radius = height / 2;
+        return System.Math.PI * radius * radius;
+      }
+    }
+  }
+
+}
