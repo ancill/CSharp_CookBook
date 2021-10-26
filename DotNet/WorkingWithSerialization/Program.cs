ccd86c92da8443be9a40f225874737ae006aa7db
Ancill <42762155+ancill@users.noreply.github.com>
2021-10-26 13:03:02 +0300

    Update Program.cs

--- DotNet/WorkingWithSerialization/Program.cs
+++ DotNet/WorkingWithSerialization/Program.cs
@@ -15,22 +15,22 @@ namespace Packt
       // ---- XML part ---------
       // create an object graph
       var people = new List<Person>
-    {
-    new Person(30000M) { FirstName = "Alice",
-    LastName = "Smith",
-    DateOfBirth = new DateTime(1974, 3, 14),
-     },
-    new Person(40000M) { FirstName = "Bob",
-     LastName = "Jones",
-    DateOfBirth = new DateTime(1969, 11, 23) },
-     new Person(20000M) { FirstName = "Charlie",
-     LastName = "Cox",
-    DateOfBirth = new DateTime(1984, 5, 4),
-    Children = new HashSet<Person>
-      { new Person(0M) { FirstName = "Sally",
+        {
+        new Person(30000M) { FirstName = "Alice",
+        LastName = "Smith",
+        DateOfBirth = new DateTime(1974, 3, 14),
+        },
+        new Person(40000M) { FirstName = "Bob",
+        LastName = "Jones",
+        DateOfBirth = new DateTime(1969, 11, 23) },
+        new Person(20000M) { FirstName = "Charlie",
         LastName = "Cox",
-        DateOfBirth = new DateTime(2000, 7, 12) } } }
-      };
+        DateOfBirth = new DateTime(1984, 5, 4),
+        Children = new HashSet<Person>
+          { new Person(0M) { FirstName = "Sally",
+            LastName = "Cox",
+            DateOfBirth = new DateTime(2000, 7, 12) } } }
+          };
       // create object that will format a List of Persons as XML
       var xs = new XmlSerializer(typeof(List<Person>)); // create a file to write to
       string path = Combine(CurrentDirectory, "people.xml");
