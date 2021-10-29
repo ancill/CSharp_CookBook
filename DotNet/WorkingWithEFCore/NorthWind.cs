f9232ff420b1f7e51273d1808eb39f507b1312ce
Ancill <42762155+ancill@users.noreply.github.com>
2021-10-29 22:15:53 +0300

    k

--- DotNet/WorkingWithEFCore/NorthWind.cs
+++ DotNet/WorkingWithEFCore/NorthWind.cs
@@ -14,7 +14,7 @@ namespace Packt.Shared
     {
       string path = System.IO.Path.Combine(System.Environment.CurrentDirectory, "Northwind.db");
 
-      optionsBuilder.UseSqlite($"Filename={path}");
+      optionsBuilder.UseLazyLoadingProxies().UseSqlite($"Filename={path}");
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -30,6 +30,9 @@ namespace Packt.Shared
       modelBuilder.Entity<Product>()
         .Property(product => product.Cost)
         .HasConversion<double>();
+      // global filter to remove discontinued products
+      modelBuilder.Entity<Product>()
+        .HasQueryFilter(p => !p.Discontinued);
     }
   }
 }
--- DotNet/WorkingWithEFCore/Program.cs
+++ DotNet/WorkingWithEFCore/Program.cs
@@ -10,9 +10,15 @@ public class Program
 {
   public static void Main()
   {
-    QueryingCategories();
+    //QueryingCategories();
     //FilteredIncludes();
     //QueryingProducts();
+    //QueryingWithLike();
+    if (AddProduct(6, "Bob's Burgers", 500M))
+    {
+      WriteLine("Add product successful.");
+    }
+    ListProducts();
   }
 
   static void QueryingCategories()
@@ -25,11 +31,39 @@ public class Program
       // a query to get all categories and their related products
 
 
-      IQueryable<Category> cats = db.Categories
-        .TagWith("Categories with Products.")
-        .Include(c => c.Products);
+      IQueryable<Category> cats = db.Categories;
+      // .Include(c => c.Products);
+
+      db.ChangeTracker.LazyLoadingEnabled = false;
+
+      Write("Enable eager loading? (Y/N): ");
+      bool eagerloading = (ReadKey().Key == ConsoleKey.Y);
+      bool explicitloading = false;
+      WriteLine();
+      if (eagerloading)
+      {
+        cats = db.Categories.Include(c => c.Products);
+      }
+      else
+      {
+        cats = db.Categories;
+        Write("Enable explicit loading? (Y/N): ");
+        explicitloading = (ReadKey().Key == ConsoleKey.Y);
+        WriteLine();
+      }
       foreach (Category c in cats)
       {
+        if (explicitloading)
+        {
+          Write($"Explicitly load products for {c.CategoryName}? (Y/N): ");
+          ConsoleKeyInfo key = ReadKey();
+          WriteLine();
+          if (key.Key == ConsoleKey.Y)
+          {
+            var products = db.Entry(c).Collection(c2 => c2.Products);
+            if (!products.IsLoaded) products.Load();
+          }
+        }
         WriteLine($"{c.CategoryName} has {c.Products.Count} products.");
       }
     }
@@ -77,6 +111,57 @@ public class Program
         WriteLine(
           "{0}: {1} costs {2:$#,##0.00} and has {3} in stock.",
           item.ProductID, item.ProductName, item.Cost, item.Stock);
+      }
+    }
+  }
+
+  static void QueryingWithLike()
+  {
+    using (var db = new Northwind())
+    {
+      var loggerFactory = db.GetService<ILoggerFactory>();
+      loggerFactory.AddProvider(new ConsoleLoggerProvider());
+      Write("Enter part of a product name: ");
+      string input = ReadLine();
+      IQueryable<Product> prods = db.Products
+        .Where(p => EF.Functions.Like(p.ProductName, $"%{input}%"));
+
+      foreach (Product item in prods)
+      {
+        WriteLine("{0} has {1} units in stock. Discontinued? {2}",
+          item.ProductName, item.Stock, item.Discontinued);
+      }
+    }
+  }
+  static bool AddProduct(int categoryID, string productName, decimal? price)
+  {
+    using (var db = new Northwind())
+    {
+      var newProduct = new Product
+      {
+        CategoryID = categoryID,
+        ProductName = productName,
+        Cost = price
+      };
+      // mark product as added in change tracking
+      db.Products.Add(newProduct);
+      // save tracked change to database
+      int affected = db.SaveChanges();
+      return (affected == 1);
+    }
+  }
+
+  static void ListProducts()
+  {
+    using (var db = new Northwind())
+    {
+      WriteLine("{0,-3} {1,-35} {2,8} {3,5} {4}",
+        "ID", "Product Name", "Cost", "Stock", "Disc.");
+      foreach (var item in db.Products.OrderByDescending(p => p.Cost))
+      {
+        WriteLine("{0:000} {1,-35} {2,8:$#,##0.00} {3,5} {4}",
+          item.ProductID, item.ProductName, item.Cost,
+          item.Stock, item.Discontinued);
       }
     }
   }
