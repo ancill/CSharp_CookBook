4f63d9b6f2589aad609402a62534da19b5cf6c73
Ancill <42762155+ancill@users.noreply.github.com>
2021-10-29 19:46:35 +0300

    1

--- DotNet/WorkingWithEFCore/Program.cs
+++ DotNet/WorkingWithEFCore/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+using Packt.Shared;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+public class Program
+{
+  public static void Main()
+  {
+    QueryingCategories();
+  }
+
+  static void QueryingCategories()
+  {
+    using (var db = new Northwind())
+    {
+      WriteLine("Categories and how many products they have:");
+      // a query to get all categories and their related products
+      IQueryable<Category> cats = db.Categories
+        .Include(c => c.Products);
+      foreach (Category c in cats)
+      {
+        WriteLine($"{c.CategoryName} has {c.Products.Count} products.");
+      }
+    }
+  }
+
+  static void FilteredIncludes()
+  {
+    using (var db = new Northwind())
+    {
+      Write("Enter a minimum for units in stock: ");
+      string unitsInStock = ReadLine();
+      int stock = int.Parse(unitsInStock);
+      IQueryable<Category> cats = db.Categories
+        .Include(c => c.Products.Where(p => p.Stock >= stock));
+      foreach (Category c in cats)
+      {
+        WriteLine($"{c.CategoryName} has {c.Products.Count} products with a minimum of { stock} units in stock.");
+        foreach (Product p in c.Products)
+        {
+          WriteLine($"  {p.ProductName} has {p.Stock} units in stock.");
+        }
+      }
+    }
+  }
+}
