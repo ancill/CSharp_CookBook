4dd43403a32f1f968fb194ec938a6d900d21f62b
Ancill <42762155+ancill@users.noreply.github.com>
2021-10-30 13:58:53 +0300

    1

---
+++ Exercise/Exercise_11.2/Entities/NorthWind.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Packt.Shared
+{
+  // This manages the connection to the database
+  public class Northwind : DbContext
+  {
+    // these properties map to tables in the database
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(
+      DbContextOptionsBuilder optionsBuilder)
+    {
+      string path = System.IO.Path.Combine(System.Environment.CurrentDirectory, "Northwind.db");
+
+      optionsBuilder.UseLazyLoadingProxies().UseSqlite($"Filename={path}");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      // example of using Fluent Api instead of attributes
+      // to limit the Length of a category name to 15
+      modelBuilder.Entity<Category>()
+        .Property(category => category.CategoryName)
+        .IsRequired() // NOT NULL
+        .HasMaxLength(15);
+
+      // added to "Fix"the lack of decimal support in Sqlite
+      modelBuilder.Entity<Product>()
+        .Property(product => product.Cost)
+        .HasConversion<double>();
+      // global filter to remove discontinued products
+      modelBuilder.Entity<Product>()
+        .HasQueryFilter(p => !p.Discontinued);
+    }
+  }
+}
