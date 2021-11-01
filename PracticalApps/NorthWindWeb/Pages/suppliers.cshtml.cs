bc9d0c761616727e7eda6bfa9026993b5523d386
Ancill <42762155+ancill@users.noreply.github.com>
2021-11-01 20:39:38 +0300

    Update suppliers.cshtml.cs

--- PracticalApps/NorthWindWeb/Pages/suppliers.cshtml.cs
+++ PracticalApps/NorthWindWeb/Pages/suppliers.cshtml.cs
@@ -1,16 +1,36 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
+using System.Linq;
+using Packt.Shared;
+using Microsoft.AspNetCore.Mvc;
 namespace NorthwindWeb.Pages
 {
   public class SuppliersModel : PageModel
   {
+    private Northwind db;
     public IEnumerable<string> Suppliers { get; set; }
+
+    [BindProperty]
+    public Supplier Supplier { get; set; }
+
+    public SuppliersModel(Northwind injectedContext)
+    {
+      db = injectedContext;
+    }
     public void OnGet()
     {
       ViewData["Title"] = "Northwind Web Site - Suppliers";
-      Suppliers = new[] {
-        "Alpha Co", "Beta Limited", "Gamma Corp"
-};
+      Suppliers = db.Suppliers.Select(s => s.CompanyName);
+    }
+    public IActionResult OnPost()
+    {
+      if (ModelState.IsValid)
+      {
+        db.Suppliers.Add(Supplier);
+        db.SaveChanges();
+        return RedirectToPage("/suppliers");
+      }
+      return Page();
     }
   }
 }
