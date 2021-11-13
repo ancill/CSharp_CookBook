d17bbdc75e4bff9c4728a493b73999b3fecc1c74
Ancill <42762155+ancill@users.noreply.github.com>
2021-11-13 20:21:14 +0300

    1

---
+++ PracticalApps/Northwind.WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Northwind.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
+    [HttpGet("{days:int}")]
+    public IEnumerable<WeatherForecast> Get(int days)
+    {
+        return Enumerable.Range(1, days).Select(i =>
+         new WeatherForecast
+         {
+             Date = DateTime.Now.AddDays(i),
+             TemperatureC = Random.Shared.Next(-20, 55),
+             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+         }).ToArray();
+    }
+}
+
---
+++ PracticalApps/Northwind.WebApi/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.OpenApi.Models;
+using Packt.Shared;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddNorthwindContext();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Northwind.WebApi", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Northwind.WebApi v1"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
