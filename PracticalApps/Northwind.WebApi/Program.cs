1cad07743e2314870078e36ac54f208a4bcc321a
Ancill <42762155+ancill@users.noreply.github.com>
2021-11-14 23:56:17 +0300

    Update Program.cs

--- PracticalApps/Northwind.WebApi/Program.cs
+++ PracticalApps/Northwind.WebApi/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.OpenApi.Models;
 using Packt.Shared;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Packt.Shared; // AddNorthwindContext extension method
+using static System.Console;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +12,7 @@ builder.Services.AddControllers();
 builder.Services.AddNorthwindContext();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new() { Title = "Northwind.WebApi", Version = "v1" });
+  c.SwaggerDoc("v1", new() { Title = "Northwind.WebApi", Version = "v1" });
 });
 
 var app = builder.Build();
@@ -17,8 +20,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Northwind.WebApi v1"));
+  app.UseSwagger();
+  app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Northwind.WebApi v1"));
 }
 
 app.UseHttpsRedirection();
