0c77f5509b743f6461b6fff857d9b34bd5f102c2
Ancill <42762155+ancill@users.noreply.github.com>
2021-11-18 09:02:58 +0300

    Update CustomerController.cs

--- PracticalApps/Northwind.WebApi/Controllers/CustomerController.cs
+++ PracticalApps/Northwind.WebApi/Controllers/CustomerController.cs
@@ -31,4 +31,71 @@ public class CustomerController : ControllerBase
         .Where(customer => customer.Country == country);
     }
   }
+  // GET: api/customers/[id]
+  [HttpGet("{id}", Name = nameof(GetCustomer))]
+  [ProducesResponseType(200, Type = typeof(Customer))]
+  [ProducesResponseType(404)]
+  public async Task<IActionResult> GetCustomer(string id)
+  {
+    Customer? c = await repo.RetrieveAsync(id);
+    if (c == null)
+    {
+      return NotFound(); // 404 Resource not found
+    }
+    return Ok(c);
+  }
+
+  // GET: api/customers
+  // Body: Customer (JSON, XML)
+  [HttpPost]
+  [ProducesResponseType(201, Type = typeof(Customer))]
+  [ProducesResponseType(400)]
+  public async Task<IActionResult> Create([FromBody] Customer c)
+  {
+    if (c == null)
+    {
+      return BadRequest(); // 400 bad request
+    }
+
+    Customer? addedCustomer = await repo.CreateAsync(c);
+    if (addedCustomer == null)
+    {
+      return BadRequest("Repository failed to create customer.");
+    }
+    else
+    {
+      return CreatedAtRoute(
+        routeName: nameof(GetCustomer),
+        routeValues: new { id = addedCustomer.CustomerId.ToLower() },
+        value: addedCustomer);
+    }
+  }
+
+  // PUT: api/customers/[id]
+  // BODY: Customer (JSON, XML)
+  [HttpPut("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
+  public async Task<IActionResult> Update(string id, [FromBody] Customer c)
+  {
+    id = id.ToUpper();
+    c.CustomerId = c.CustomerId.ToUpper();
+    if (c == null || c.CustomerId != id)
+    {
+      return BadRequest(); // 400 bad request
+    }
+    await repo.UpdateAsync(id, c);
+    return new NoContentResult(); // 204 NO content
+  }
+
+  // DELETE: api/customers/[id]
+  [HttpDelete("{id}")]
+  [ProducesResponseType(204)]
+  [ProducesResponseType(400)]
+  [ProducesResponseType(404)]
+
+
+
+
 }
