d8c57be0a0b149d64cd0d357f0f975f835cec8ae
Ancill <42762155+ancill@users.noreply.github.com>
2021-11-27 08:43:46 +0300

    Update Program.cs

--- PracticalApps/Northwind.BlazorServer/Program.cs
+++ PracticalApps/Northwind.BlazorServer/Program.cs
@@ -14,9 +14,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  app.UseExceptionHandler("/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -26,7 +26,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+app.MapBlazorHub(); s
+ app.MapFallbackToPage("/_Host");
 
 app.Run();
